c8afa49f0fb316d185686f97678954b32382e934
jc345932 <junnao.luo@my.jcu.edu.au>
2017-08-03 13:30:58 +0800

    Dice class and Q1

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,16 @@
+import java.util.Random;
+
 public class Dice {
+    private int value;
+    private Random rand;
+    public Dice() {
+        rand = new Random();
+        value = 1;
+    }
+    public void roll (){
+        value = rand.nextInt(6)+1;
+    }
+    public int getValue(){
+        return value;
+    }
 }
---
+++ src/Q1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jc345932 on 3/08/17.
+ */
+public class Q1 {
+    public static void main(String[] args){
+
+    }
+}
